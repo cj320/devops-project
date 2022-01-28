167b11faced9efbba3d1cd46de1cdc3b968d1bfc
Carter James <carterjames6316@gmail.com>
2022-01-27 21:28:28 -0500

    Modified code to deploy to aws

--- app/src/reddit/settings.py
+++ app/src/reddit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG= True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [config('VIRTUAL_HOST'), '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
